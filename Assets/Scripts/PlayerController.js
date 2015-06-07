a8545fe59f240b4c0444a282fa9baceb575548aa
yuichiro12 <talcnrhyme@gmail.com>
2015-06-07 20:27:45 +0900

    add rotate and move

---
+++ Assets/Scripts/PlayerController.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var x : float = Input.GetAxis("Horizontal");
+	var z : float = Input.GetAxis("Vertical");
+	transform.Rotate(Vector3(z * 10, 0, x * -10), Space.World);
+}
