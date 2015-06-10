615b0b024733aa0d258a6f10d269c317c2810e14
yuichiro12 <talcnrhyme@gmail.com>
2015-06-11 03:57:53 +0900

    let player jump

--- Assets/Scripts/PlayerMoveController.js
+++ Assets/Scripts/PlayerMoveController.js
@@ -1,12 +1,26 @@
 ﻿#pragma strict
 
-function Start () {
+var rb: Rigidbody;
+var jump : boolean = false;
+var JumpInitialVelocity : float = 2.0;
 
+function Start () {
+	rb = GetComponent.<Rigidbody>();
 }
 
 function Update () {
 	var x : float = Input.GetAxis("Horizontal");
-	var y : float = Input.GetAxis("Jump");
+	if(!jump && Input.GetButtonDown("Jump")){
+		jump = true;
+		rb.velocity.y += JumpInitialVelocity;
+		rb.velocity = Vector3(0, 10, 0);
+	};
 	var z : float = Input.GetAxis("Vertical");
-	transform.Translate(Vector3(x * 0.15, y, z * 0.15), Space.World);
+	transform.Translate(Vector3(x * 0.15, 0, z * 0.15), Space.World);
+}
+
+function OnCollisionEnter (collision: Collision) {
+	if (collision.gameObject.name == "Ground"){
+		jump = false;
+	}
 }
