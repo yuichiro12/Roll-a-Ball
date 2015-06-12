cdc21ac61340417f7ac2facf01ae3042afcef215
yuichiro12 <talcnrhyme@gmail.com>
2015-06-13 04:14:31 +0900

    improve jump action

--- Assets/Scripts/PlayerMoveController.js
+++ Assets/Scripts/PlayerMoveController.js
@@ -2,7 +2,10 @@
 
 var rb: Rigidbody;
 var jump : boolean = false;
-var JumpInitialVelocity : float = 2.0;
+var jumpable : boolean = true;
+var bound : int = 0;
+var dx : float = 0;
+var dz : float = 0;
 
 function Start () {
 	rb = GetComponent.<Rigidbody>();
@@ -11,22 +14,45 @@ function Start () {
 function Update () {
 	var x : float = Input.GetAxis("Horizontal");
 	var z : float = Input.GetAxis("Vertical");
-	if(!jump && Input.GetButtonDown("Jump")){
+	transform.Rotate(Vector3(z * 10, 0, x * -10), Space.World);
+
+	if (!jump && jumpable && Input.GetButtonDown("Jump")) {
+		dx = x;
+		dz = z;
+
 		jump = true;
-		rb.velocity.y += JumpInitialVelocity;
-		rb.velocity = Vector3(x * 0.15 * (Time.deltaTime * 30), 10, z * 0.15 *  (Time.deltaTime * 30));
-	};
-	transform.Translate(Vector3(x * 0.15, 0, z * 0.15), Space.World);
+		rb.velocity = Vector3(0, 20, 0);
+	}
+
+	if (jump) {
+		transform.Translate(Vector3((x/1.5 + dx)/2 * 0.15, 0, (z/1.5 + dz)/2 * 0.15), Space.World);
+		transform.Rotate(Vector3((z/6 + dz)/2 * 10, 0, (x/6 + dx)/2 * -10), Space.World);
+	} else if (bound != 0) {
+		transform.Translate(Vector3((x/1.2 + dx/1.5)/2 * 0.15, 0, (z/1.2 + dz/1.5)/2 * 0.15), Space.World);
+		transform.Rotate(Vector3((z/6 + dz/bound)/2 * 10, 0, (x/6 + dx/bound)/2 * -10), Space.World);
+	} else {
+		transform.Translate(Vector3(x * 0.15, 0, z * 0.15), Space.World);
+	}
 }
 
 function OnCollisionEnter (collision: Collision) {
-	if (collision.gameObject.name == "Ground"){
+	if (collision.gameObject.name == "Ground") {
 		jump = false;
+		jumpable = true;
 	}
 }
 
 function OnCollisionExit (collision: Collision) {
-	if (collision.gameObject.name == "Ground"){
-		jump = true;
+	if (!jump && collision.gameObject.name == "Ground") {
+		bound++;
+		yield WaitForSeconds(0.03);
+		jumpable = false;
+	}
+}
+
+function OnCollisionStay (collision: Collision) {
+	if (collision.gameObject.name == "Ground") {
+		bound = 0;
+		jumpable = true;
 	}
 }
--- Assets/Scripts/PlayerRotateController.js
+++ Assets/Scripts/PlayerRotateController.js
@@ -5,7 +5,4 @@ function Start () {
 }
 
 function Update () {
-	var x : float = Input.GetAxis("Horizontal");
-	var z : float = Input.GetAxis("Vertical");
-	transform.Rotate(Vector3(z * 10, 0, x * -10), Space.World);
 }
